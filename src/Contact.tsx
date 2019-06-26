34e45ca1a73da15b264870d8e5006c9c5be9c272
AppShore <git@appshore.com>
2019-06-26 15:31:02 +0100

    GQL queries and mutations

---
+++ src/Contact.tsx
@@ -0,0 +1,36 @@
+import gql from 'graphql-tag';
+
+const DELETE_CONTACT = gql`
+  mutation contactDelete( $id: ID!) {
+    deleteContact( id: $id)
+  }
+`;
+
+const GET_CONTACTS = gql`
+  query contactsList {
+    contacts {
+      name
+      email
+      id
+    }
+  }
+`;
+
+const SAVE_CONTACT = gql`
+  mutation contactSave( $contact: Contact!) {
+    addContact( contact: $contact)
+  }
+`;
+
+const UPDATE_CONTACT = gql`
+  mutation contactUpdate( $contact: Contact!) {
+    updateContact( contact: $contact)
+  }
+`;
+
+export {
+  DELETE_CONTACT,
+  GET_CONTACTS,
+  SAVE_CONTACT,
+  UPDATE_CONTACT
+}
