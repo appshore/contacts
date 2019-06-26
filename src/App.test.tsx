9784a9faab451b9923c128aba91a97db4d7cf592
AppShore <git@appshore.com>
2019-06-26 15:43:20 +0100

    review initial test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
 import App from './App';
 
 it('renders without crashing', () => {
+  const apolloClient = new ApolloClient({
+    cache: new InMemoryCache(), 
+    link: new HttpLink({
+      uri: 'http://localhost:3001/'
+    })
+  })
+
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>, 
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
