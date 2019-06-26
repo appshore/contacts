17ee2531b7b9839808e955032b7a7126e3d831d7
AppShore <git@appshore.com>
2019-06-26 15:29:49 +0100

    add Apollo client for react

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const apolloClient = new ApolloClient({
+  cache: new InMemoryCache(), 
+  link: new HttpLink({
+    uri: 'http://localhost:3001/'
+  })
+})
+
+ReactDOM.render(
+  <ApolloProvider client={apolloClient}>
+    <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
