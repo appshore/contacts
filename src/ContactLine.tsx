2a33b685f5a40a8e646ff89a1f98089257b454eb
AppShore <git@appshore.com>
2019-06-28 12:02:26 +0100

    review links

--- src/ContactLine.tsx
+++ src/ContactLine.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 
+import EditIcon from '@material-ui/icons/Edit';
+
 import ContactDelete from './ContactDelete';
-import ContactEdit from './ContactEdit';
 import { ContactInterface } from './Contact.d';
 
 const ContactLine = ({ name, email, id }: ContactInterface) => {
@@ -16,7 +18,7 @@ const ContactLine = ({ name, email, id }: ContactInterface) => {
         <ListItemText id={id} primary={`${name} (${email})`} />
       </Link>
       <ListItemSecondaryAction>
-        <ContactEdit id={id} />
+        <Link to={`/contact/${id}/edit`}><IconButton edge="end"><EditIcon /></IconButton></Link>
         <ContactDelete id={id} />
       </ListItemSecondaryAction>
     </ListItem>
